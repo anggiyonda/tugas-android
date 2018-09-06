0db779ea3d5301ce591cd14fa081b4f6134b650b
anggiyonda <43041078+anggiyonda@users.noreply.github.com>
2018-09-06 22:30:33 +0700

    Add files via upload

---
+++ JavaApplication5 PERKENALAN/src/javaapplication5/perkenalan/JavaApplication5PERKENALAN.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication5.perkenalan;
+
+/**
+ *
+ * @author ASUS
+ */
+public class JavaApplication5PERKENALAN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+    int jumlah = 5;
+    for (int i=1 ; i<=jumlah ; i++){
+        for (int j=0 ; j < i ; j++){
+            System.out.print("*");    
+        }
+    System.out.println(" ");
+    }
+    }
+}
+   
+
